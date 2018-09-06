7d647e36c1310fffe3f0c1e49e60f017734f6cde
Alistair Skelton <35896420+abskelton@users.noreply.github.com>
2018-09-06 15:42:03 +0200

    update

--- index.php
+++ index.php
@@ -19,193 +19,149 @@ $container['view'] = function() {
 
 // Initialize database
 $container['db'] = function() {
-    return new PDO('sqlite:ridesharing.sqlite');
+    return new PDO('sqlite:survey.sqlite');
 };
 
-// Load and initialize MesageBird SDK
-$container['messagebird'] = function() {
-    return new MessageBird\Client(getenv('MESSAGEBIRD_API_KEY'));
+// Load survey questions from JSON file
+$container['questions'] = function() {
+    return json_decode(file_get_contents(__DIR__.'/questions.json'), true);
 };
 
-// Show admin interface
-$app->get('/', function($request, $response) {
-    // Find proxy numbers
-    $stmt = $this->db->query('SELECT number FROM proxy_numbers');
-    $proxyNumbers = $stmt->fetchAll();
-
-    // Find current rides
-    $stmt = $this->db->query('SELECT c.name AS customer, d.name AS driver, start, destination, datetime, p.number AS number FROM rides r JOIN customers c ON c.id = r.customer_id JOIN drivers d ON d.id = r.driver_id JOIN proxy_numbers p ON p.id = r.number_id');
-    $rides = $stmt->fetchAll();
-
-    // Collect customers
-    $stmt = $this->db->query('SELECT * FROM customers');
-    $customers = $stmt->fetchAll();
-
-    // Collect drivers
-    $stmt = $this->db->query('SELECT * FROM drivers');
-    $drivers = $stmt->fetchAll();
-
-    // Render template
-    return $this->view->render($response, 'admin.html.twig', [
-        'proxy_numbers' => $proxyNumbers,
-        'rides' => $rides,
-        'customers' => $customers,
-        'drivers' => $drivers
-    ]);
+// Create middleware that determines correct base URL
+$app->add(function($request, $response, $next) {
+    $this->router->setBasePath(
+        (($request->getUri()->getScheme() == 'https' || $request->getHeaderLine('X-Forwarded-Proto') == 'https') ? 'https' : 'http')
+        . '://' . $request->getUri()->getHost()
+    );    
+    return $next($request, $response);
 });
 
-// Create a new ride
-$app->post('/createride', function($request, $response) {
-    // Find customer details
-    $stmt = $this->db->prepare('SELECT * FROM customers WHERE id = :id');
-    $stmt->execute([ 'id' => $request->getParsedBodyParam('customer') ]);
-    $customer = $stmt->fetch();
+/**
+ * Helper function to generate a "say" call flow step.
+ */
+function say($payload) {
+    return [
+        'action' => 'say',
+        'options' => [
+            'payload' => $payload,
+            'voice' => 'male',
+            'language' => 'en-US'
+        ]
+    ];
+}
 
-    // Find driver details
-    $stmt = $this->db->prepare('SELECT * FROM drivers WHERE id = :id');
-    $stmt->execute([ 'id' => $request->getParsedBodyParam('driver') ]);
-    $driver = $stmt->fetch();
-
-    // Find a number that has not been used by the driver or the customer
-    $stmt = $this->db->prepare('SELECT * FROM proxy_numbers '
-        . 'WHERE id NOT IN (SELECT number_id FROM rides WHERE customer_id = :customer) '
-        . 'AND id NOT IN (SELECT number_id FROM rides WHERE driver_id = :driver)');
-    $stmt->execute([
-        'customer' => $customer['id'],
-        'driver' => $driver['id']
-    ]);
-    $proxyNumber = $stmt->fetch();
-
-    if ($proxyNumber === false) {
-        // No number found!
-        return "No number available! Please extend your pool.";
-    }
-
-    // Store ride in database
-    $stmt = $this->db->prepare('INSERT INTO rides (start, destination, datetime, customer_id, driver_id, number_id) VALUES (:start, :destination, :datetime, :customer, :driver, :number)');
-    $stmt->execute([
-        'start' => $request->getParsedBodyParam('start'),
-        'destination' => $request->getParsedBodyParam('destination'),
-        'datetime' => $request->getParsedBodyParam('datetime'),
-        'customer' => $customer['id'],
-        'driver' => $driver['id'],
-        'number' => $proxyNumber['id']
-    ]);
-        
-    // Prepare message object
-    $message = new MessageBird\Objects\Message;
-    $message->originator = $proxyNumber['number'];
-    
-    // Notify the customer
-    $message->recipients = [ $customer['number'] ];
-    $message->body = $driver['name'] . " will pick you up at " . $request->getParsedBodyParam('datetime') . ". Reply to this message or call this number to contact the driver.";
-    try {
-        $this->messagebird->messages->create($message);
-    } catch (Exception $e) {
-        error_log(get_class($e).": ".$e->getMessage());
-    }
-
-    // Notify the driver
-    $message->recipients = [ $driver['number'] ];
-    $message->body = $customer['name'] . " will wait for you at " . $request->getParsedBodyParam('datetime') . ". Reply to this message or call this number to contact the customer.";
-    try {
-        $this->messagebird->messages->create($message);
-    } catch (Exception $e) {
-        error_log(get_class($e).": ".$e->getMessage());
-    }
-
-    // Redirect back to previous view
-    return $response->withRedirect('/');
-});
-
-// Handle incoming messages
-$app->post('/webhook', function($request, $response) {
-    // Read input sent from MessageBird
-    $number = $request->getParsedBodyParam('originator');
-    $text = $request->getParsedBodyParam('payload');
-    $proxy = $request->getParsedBodyParam('recipient');
-
-    // Find potential rides that fit the numbers
-    $stmt = $this->db->prepare('SELECT c.number AS customer_number, d.number AS driver_number, p.number AS proxy_number '
-        . 'FROM rides r JOIN customers c ON r.customer_id = c.id JOIN drivers d ON r.driver_id = d.id JOIN proxy_numbers p ON p.id = r.number_id '
-        . 'WHERE proxy_number = :proxy AND (driver_number = :number OR customer_number = :number)');
-    $stmt->execute([
-        'number' => $number,
-        'proxy' => $proxy
-    ]);
-    $row = $stmt->fetch();
-
-    if ($row !== false) {
-        // Got a match!
-
-        // Prepare message object
-        $message = new MessageBird\Objects\Message;
-        $message->originator = $proxy;
-        $message->body = $text;
-
-        // Need to find out whether customer or driver sent this and forward to the other side
-        if ($number == $row['customer_number'])
-            $message->recipients = [ $row['driver_number'] ];
-        else
-        if ($number == $row['driver_number'])
-            $message->recipients = [ $row['customer_number'] ];
-                
-        // Forward the message through the MessageBird API
-        try {
-            $this->messagebird->messages->create($message);
-            error_log("Forwarded text from " . $number . " to " . $message->recipients[0]);
-        } catch (Exception $e) {
-            error_log(get_class($e).": ".$e->getMessage());
-        }
-    } else {
-        // Cannot match numbers
-        error_log("Could not find a ride for customer/driver " . $number . " that uses proxy " . $proxy . ".");
-    }
-
-    // Return any response, MessageBird won't parse this
-    return "OK";
-});
-
-// Handle incoming calls
-$app->get('/webhook-voice', function($request, $response) {
-    // Read input sent from MessageBird
+$app->map(['GET','POST'], '/callStep', function($request, $response) {
+    // Read query input sent from MessageBird
+    $callId = $request->getQueryParam('callID');
     $number = $request->getQueryParam('source');
-    $proxy = $request->getQueryParam('destination');
 
-    // Answer will always be XML
-    $response = $response->withHeader('Content-Type', 'application/xml')
-        ->write('<?xml version="1.0" encoding="UTF-8"?>');
+    // Prepare a Call Flow that can be extended
+    $flow = [
+        'title' => "Survey Call Step",
+        'steps' => []
+    ];
 
-    // Find potential rides that fit the numbers
-    $stmt = $this->db->prepare('SELECT c.number AS customer_number, d.number AS driver_number, p.number AS proxy_number '
-        . 'FROM rides r JOIN customers c ON r.customer_id = c.id JOIN drivers d ON r.driver_id = d.id JOIN proxy_numbers p ON p.id = r.number_id '
-        . 'WHERE proxy_number = :proxy AND (driver_number = :number OR customer_number = :number)');
-    $stmt->execute([
-        'number' => $number,
-        'proxy' => $proxy
-    ]);
-    $row = $stmt->fetch();
+    // Find a database entry for the call
+    $stmt = $this->db->prepare('SELECT * FROM participants WHERE callId = :callId');
+    $stmt->execute([ 'callId' => $callId ]);
+    $participant = $stmt->fetch();
 
-    if ($row !== false) {
-        // Got a match!
-        
-        // Need to find out whether customer or driver sent this and forward to the other side
-        $destination = "";
-        if ($number == $row['customer_number'])
-            $destination = $row['driver_number'];
-        else
-        if ($number == $row['driver_number'])
-            $destination = $row['customer_number'];
-                
-        // Create call flow to instruct transfer
-        error_log("Transferring call to " . $destination);
-        $response->write('<Transfer destination="' . $destination . '" mask="true" />');
+    if ($participant === false) {
+        // Create new participant database entry
+        $stmt = $this->db->prepare('INSERT INTO participants (callId, number) VALUES (:callId, :number)');
+        $stmt->execute([ 'callId' => $callId, 'number' => $number ]);
+        error_log("Created participant.");
+
+        // The person is just starting the survey
+        $questionId = 0;
     } else {
-        // Cannot match numbers
-        $response->write('<Say language="en-GB" voice="female">Sorry, we cannot identify your transaction. Make sure you call in from the number you registered.</Say>');
+        // Participant already exists,
+        // determine the next question
+        $stmt = $this->db->prepare('SELECT COUNT(*) FROM responses WHERE participant_id = :participantId');
+        $stmt->execute([ 'participantId' => $participant['id'] ]);
+
+        $questionId = (int)$stmt->fetchColumn() + 1;
+
+        // Store the response of the previous question
+        $jsonInput = json_decode($request->getBody()->getContents(), true);
+        $stmt = $this->db->prepare('INSERT INTO responses (participant_id, legId, recordingId) VALUES (:participantId, :legId, :recordingId)');
+        $stmt->execute([ 'participantId' => $participant['id'],
+            'legId' => $jsonInput['legId'],
+            'recordingId' => $jsonInput['id'] ]);
+        error_log("Added response.");
     }
 
-    return $response;
+    if ($questionId == count($this->questions)) {
+        // All questions have been answered
+        $flow['steps'][] = say("You have completed our survey. Thank you for participating!");
+    } else {
+        if ($questionId == 0) {
+            // Before first question, say welcome
+            $flow['steps'][] = say("Welcome to our survey! You will be asked " . count($this->questions) . " questions. The answers will be recorded. Speak your response for each and press any key on your phone to move on to the next question. Here is the first question:");
+        }
+        // Ask next question
+        $flow['steps'][] = say($this->questions[$questionId]);
+
+        // Request recording of question
+        $flow['steps'][] = [
+            'action' => 'record',
+            'options' => [
+                // Finish either on key press or after 10 seconds of silence
+                'finishOnKey' => 'any',
+                'timeout' => 10,
+                // Send recording to this same call flow URL
+                'onFinish' => $this->router->pathFor('callStep')
+            ]
+        ];
+    }
+
+    // Return flow as JSON response
+    return $response->withJson($flow);
+
+})->setName('callStep');
+
+$app->get('/admin', function($request, $response) {
+    $participants = [];
+    
+    $query = $this->db->query('SELECT * FROM participants');
+    $participantsFromDb = $query->fetchAll();
+    foreach ($participantsFromDb as $p) {
+        $responses = [];
+        
+        $stmt = $this->db->query('SELECT * FROM responses WHERE participant_id = :participantId');
+        $stmt->execute([ 'participantId' => $p['id'] ]);
+        $responsesFromDb = $stmt->fetchAll();
+        
+        for ($i = 0; $i < count($responsesFromDb); $i++) {
+            $responses[] = [
+                'question' => $this->questions[$i],
+                'legId' => $responsesFromDb[$i]['legId'],
+                'recordingId' => $responsesFromDb[$i]['recordingId']
+            ];
+        }
+
+        $participants[] = [
+            'callId' => $p['callId'],
+            'number' => $p['number'],
+            'responses' => $responses
+        ];
+    }
+    
+    return $this->view->render($response, 'admin.html.twig', [
+        'participants' => $participants
+    ]);
+});
+
+$app->get('/play/{callId}/{legId}/{recordingId}', function($request, $response, $params) {
+    // Make a proxy request to the audio file on the API
+    $client = new GuzzleHttp\Client;
+    return $client->get('https://voice.messagebird.com/calls/' . $params['callId']
+        . '/legs/' . $params['legId']
+        . '/recordings/' . $params['recordingId'] . '.wav', [
+            'headers' => [
+                'Authorization' => 'AccessKey '.getenv('MESSAGEBIRD_API_KEY')
+            ]
+        ]);
 });
 
 // Start the application
--- init.php
+++ init.php
@@ -1,31 +1,8 @@
 <?php
 
 // Create/open the database file
-$db = new PDO('sqlite:ridesharing.sqlite');
+$db = new PDO('sqlite:survey.sqlite');
 
-// Create the data model
-
-// Customers: have ID, name and number
-$db->exec('CREATE TABLE customers (id INTEGER PRIMARY KEY, name TEXT, number TEXT)');
-// Drivers: have ID, name and number
-$db->exec('CREATE TABLE drivers (id INTEGER PRIMARY KEY, name TEXT, number TEXT)');
-// Proxy Numbers: have ID and number
-$db->exec('CREATE TABLE proxy_numbers (id INTEGER PRIMARY KEY, number TEXT)');
-// Rides: have ID, start, destination and date; are connected to a customer, a driver, and a proxy number
-$db->exec('CREATE TABLE rides (id INTEGER PRIMARY KEY, start TEXT, destination TEXT, datetime TEXT, customer_id INTEGER, driver_id INTEGER, number_id INTEGER, FOREIGN KEY (customer_id) REFERENCES customers(id), FOREIGN KEY (driver_id) REFERENCES drivers(id))');
-
-// Insert some data
-    
-// Create a sample customer for testing
-// -> enter your name and number here!
-$db->exec('INSERT INTO customers (name, number) VALUES ("Caitlyn Carless", "31970XXXX")');
-
-// Create a sample driver for testing
-// -> enter your name and number here!
-$db->exec('INSERT INTO drivers (name, number) VALUES ("David Driver", "31970YYYY")');
-    
-// Create a proxy number
-// -> provide a number purchased from MessageBird here
-// -> copy the line if you have more numbers
-$db->exec('INSERT INTO proxy_numbers (number) VALUES ("31970ZZZZ")');
-
+// Generate schema: 2 tables
+$db->exec('CREATE TABLE participants (id INTEGER PRIMARY KEY, callId TEXT, number TEXT)');
+$db->exec('CREATE TABLE responses (id INTEGER PRIMARY KEY, participant_id INTEGER, legId TEXT, recordingId TEXT, FOREIGN KEY (participant_id) REFERENCES participants(id))');
